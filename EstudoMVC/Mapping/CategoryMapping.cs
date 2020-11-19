038097a3946f916415205f54969712836a35bee9
Marcelo Marques Lima <marcelomarqueslima.dev@gmail.com>
2020-11-19 12:25:32 -0300

    Projeto de estudo C# .NET 5 com SQL Server e ORM EntityFrameork

---
+++ EstudoMVC/Mapping/CategoryMapping.cs
@@ -0,0 +1,23 @@
+﻿using EstudoMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EstudoMVC.Mapping
+{
+    public class CategoryMapping : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(250)
+                ;
+        }
+    }
+}
